60f3aa9c7459e06395499aa886009be2e2cf1820
Gabriele Felice <gabriele_felice@hotmail.com>
2023-01-04 22:36:55 -0300

    Aula 2

---
+++ AppContas.Web/AppContas.Web/Requests/CadastrarContaRequest.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppContas.Web.Requests
+{
+    public class CadastrarContaRequest
+    {
+        [MinLength(8, ErrorMessage = "Informe no mínimo {1} caracteres.")]
+        [MaxLength(150, ErrorMessage = "Informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe o nome da conta.")]
+        public string? NomeConta { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a data da conta.")]
+        public string? DataConta { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o valor da conta.")]
+        public string? Valor { get; set; }
+
+        [MinLength(8, ErrorMessage = "Informe no mínimo {1} caracteres.")]
+        [MaxLength(250, ErrorMessage = "Informe no máximo {1} caracteres.")]
+        [Required(ErrorMessage = "Por favor, informe as observações da conta.")]
+        public string? Observacoes { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe o tipo da conta.")]
+        public string? TipoId { get; set; }
+
+        [Required(ErrorMessage = "Por favor, informe a categoria da conta.")]
+        public string? CategoriaId { get; set; }
+    }
+}
+
+
+
---
+++ AppContas.Web/AppContas.Web/Responses/ContaResponse.cs
@@ -0,0 +1,28 @@
+﻿namespace AppContas.Web.Responses
+{
+    public class ContaResponse
+    {
+        public Guid? Id { get; set; }
+        public string? NomeConta { get; set; }
+        public DateTime? DataConta { get; set; }
+        public decimal? Valor { get; set; }
+        public string? Observacoes { get; set; }
+        public Tipo? Tipo { get; set; }
+        public Categoria? Categoria { get; set; }
+    }
+
+    public class Tipo
+    {
+        public int? Id { get; set; }
+        public string? Nome { get; set; }
+    }
+
+    public class Categoria
+    {
+        public Guid? Id { get; set; }
+        public string? Nome { get; set; }
+    }
+}
+
+
+
